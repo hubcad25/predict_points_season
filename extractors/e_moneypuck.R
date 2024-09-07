27e26189a275a8ecbc346409be17639097d62964
hubcad25 <hubertcadieux@gmail.com>
2024-09-06 22:28:18 -0400

    changes

--- extractors/e_moneypuck.R
+++ extractors/e_moneypuck.R
@@ -19,7 +19,7 @@ for (i in seq_along(links_to_download)) {
   full_url <- paste0(base_url, links_to_download[i])
   file_name <- sub(".*/(.*)\\.csv$", "\\1", links_to_download[i]) # Extract the base name without .csv
   year <- sub(".*/(\\d{4}).*", "\\1", links_to_download[i]) # Extract the year from the link
-  destfile <- paste0("data/lake/moneypuck", file_name, "_", year, ".rds")
+  destfile <- paste0("data/lake/moneypuck/", file_name, "_", year, ".rds")
   
   data <- read.csv(full_url) # Download the CSV data
   saveRDS(data, destfile) # Save as .rds
