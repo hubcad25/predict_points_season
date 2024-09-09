37f7eb2747d4ce76d4d92599072e03d972593cf3
hubcad25 <hubertcadieux@gmail.com>
2024-09-08 22:04:35 -0400

    put variables in pts_predictR

--- loaders/l_moneypuck_skaters.R
+++ loaders/l_moneypuck_skaters.R
@@ -14,27 +14,7 @@ lake_files <- list.files(
 )
 
 ## Variables to keep in warehouse table
-variables <- c(
-  "game_score" = "gameScore",
-  "onice_xg_pct" = "onIce_xGoalsPercentage",
-  "xg_flurryscorevenue_adjusted" = "I_F_flurryScoreVenueAdjustedxGoals",
-  "xrebounds" = "I_F_xRebounds",
-  "xplay_continued" = "I_F_xPlayContinuedInZone",
-  "shots" = "I_F_shotsOnGoal",
-  "xon_goal" = "I_F_xOnGoal",
-  "xg_rebound" = "I_F_reboundxGoals",
-  "xg_lowdanger" = "I_F_lowDangerxGoals",
-  "xg_mediumdanger" = "I_F_mediumDangerxGoals",
-  "xg_highdanger" = "I_F_highDangerxGoals",
-  "xrebounds_quality" = "I_F_xGoalsFromxReboundsOfShots",
-  "earned_rebounds" = "I_F_xGoals_with_earned_rebounds_scoreFlurryAdjusted",
-  "goals_lowdanger" = "I_F_lowDangerGoals",
-  "goals_mediumdanger" = "I_F_mediumDangerGoals",
-  "goals_highdanger" = "I_F_highDangerGoals",
-  "goals_rebound" = "I_F_reboundGoals",
-  "takeaways" = "I_F_takeaways",
-  "giveaways" = "I_F_giveaways"
-)
+variables <- ptspredictR::independant_variables
 
 for (i in lake_files){
   data <- readRDS(i)
