6b922ce1d40d313ff118cde0e898475a9df9dc96
hubcad25 <hubertcadieux@gmail.com>
2024-09-08 21:20:54 -0400

    mini add

--- refiners/r_teammates_time_distribution.R
+++ refiners/r_teammates_time_distribution.R
@@ -1,6 +1,5 @@
 ## This script takes the lines dataset in the warehouse and transforms it into a dataframe containing each pair of players that played on a line together and sums up their icetime together. Then, it divides this value by the total icetime of each player, thus resulting in the teammates time distribution of each player.
 
-
 # Packages ---------------------------------------------------------------
 library(dplyr)
 
