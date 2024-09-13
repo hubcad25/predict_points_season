3b12da660039a9fe114021a7cec1bc86535583f5
hubcad25 <hubertcadieux@gmail.com>
2024-09-13 00:17:59 -0400

    train random forest

---
+++ refiners/r_train_random_forest.R
@@ -0,0 +1,51 @@
+## This script will take the data for forwards and defensemen and train a random forest
+
+# Packages ---------------------------------------------------------------
+
+# Data -------------------------------------------------------------------
+data_f <- readRDS("data/marts/data_random_forest_forwards.rds") |> 
+  dplyr::select(-points)
+data_d <- readRDS("data/marts/data_random_forest_defensemen.rds") |> 
+  dplyr::select(-points)
+
+# Forwards ---------------------------------------------------------------
+
+model_goals <- ptspredictR::train_random_forest(
+  data_f |> dplyr::select(-assists),
+  "goals"
+)
+
+randomForest::varImpPlot(model_goals)
+
+saveRDS(model_goals, "data/models/model_goals_f.rds")
+
+model_assists <- ptspredictR::train_random_forest(
+  data_f |> dplyr::select(-goals),
+  "assists"
+)
+
+randomForest::varImpPlot(model_assists)
+
+saveRDS(model_assists, "data/models/model_assists_f.rds")
+
+# Defensemen ---------------------------------------------------------------
+
+model_goals_d <- ptspredictR::train_random_forest(
+  data_d |> dplyr::select(-assists),
+  "goals"
+)
+
+randomForest::varImpPlot(model_goals_d)
+
+saveRDS(model_goals_d, "data/models/model_goals_d.rds")
+
+model_assists_d <- ptspredictR::train_random_forest(
+  data_d |> dplyr::select(-goals),
+  "assists"
+)
+
+randomForest::varImpPlot(model_assists_d)
+
+saveRDS(model_assists_d, "data/models/model_assists_d.rds")
+
+
