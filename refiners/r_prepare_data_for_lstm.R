d52093b82aa173bb0c119cfdc144ce774e35cabf
hubcad25 <hubertcadieux@gmail.com>
2024-09-22 15:23:22 -0400

    test_lstm

--- refiners/r_prepare_data_for_lstm.R
+++ refiners/r_prepare_data_for_lstm.R
@@ -1,3 +1,5 @@
+## This script takes the data that is used to train the random forest models to prepares them for the lstm models that are going to be used to project IVs for the next season and thus use the random forest models to predict the points of a player. It saves data for all players into a json file
+
 # Packages ---------------------------------------------------------------
 library(dplyr)
 
@@ -7,11 +9,89 @@ data <- rbind(
   readRDS("data/marts/data_random_forest_defensemen.rds")
 )
 
-variables <- c(ptspredictR::ev_variables, names(ptspredictR::pp_variables))
+### Player infos for fixed variables: age, height, draft_rank, country
+df_player_infos <- readRDS("data/warehouse/player_infos.rds") |> 
+  mutate(
+    forward = ifelse(position %in% c("C", "L", "R"), 1, 0),
+    defenseman = ifelse(position == "D", 1, 0)
+) |> 
+  select(player_id, yob, height, draft_rank, forward, defenseman)
 
-df_past <- ptspredictR::build_past_stats_df(
+# Set variables to include in past stats ----------------------------------------------------------
+variables <- c(
+  "ev_icetime", "pp_icetime",
+  ptspredictR::ev_variables,
+  names(ptspredictR::pp_variables)
+)
+
+# Build past stats dataframe ---------------------------------------------
+df_past_stats <- ptspredictR::build_past_stats_df(
   data = data,
   variables = variables,
-  window = 5
-)
+  window = 3
+) %>%
+  # Add player infos
+  left_join(., df_player_infos, by = "player_id") |> 
+  filter(season >= 2011) |> 
+  mutate(age = season - yob) |> 
+  select(-yob)
+
+# Add targets and save as big json ---------------------------------------
+path <- "data/marts/lstm_data/"
+
+for (i in variables){
+  ## Add target
+  df_target_only <- data |>
+    select(player_id, season, all_of(i))
+  df_with_target <- left_join(
+    df_past_stats,
+    df_target_only,
+    by = c("player_id", "season")
+  )
+  ptspredictR::save_df_past_stats_as_json(
+    df_past_stats = df_with_target,
+    output_file = paste0(path, i, ".rds")
+  )
+}
+
+saveRDS(df_with_target, file = "data/warehouse/test_lstm.rds")
+
+# Fonction pour structurer les past_stats en vecteurs de 3 éléments dans une liste
+get_past_stats_as_vector <- function(past_stats) {
+  sapply(past_stats[[1]], function(x) as.vector(x), simplify = FALSE)
+}
+
+# Fonction principale pour générer le JSON
+generate_json <- function(df) {
+  # Initialisation d'une liste pour stocker les données finales
+  players_list <- list()
+  
+  # Boucle à travers chaque ligne du dataframe
+  for (i in seq_len(nrow(df))) {
+    player_id <- paste0("player", i)
+    
+    # Extraction des past_stats sous forme de vecteurs de 3 éléments
+    past_stats_vector <- get_past_stats_as_vector(df$past_stats[i])
+    
+    # Création d'un dictionnaire pour le joueur
+    player_data <- list(
+      past_stats = past_stats_vector,
+      target = df$target[i],
+      age = df$age[i],
+      height = df$height[i]
+    )
+    
+    # Ajout des données du joueur à la liste finale
+    players_list[[player_id]] <- player_data
+    message(i)
+  }
+  
+  # Conversion de la liste en JSON
+  json_output <- jsonlite::toJSON(players_list)
+  
+  return(json_output)
+}
+
+json_result <- generate_json(df_with_target)
+
 
---
+++ refiners/r_test_lstm.R
@@ -0,0 +1,44 @@
+library(dplyr)
+library(keras)
+library(tensorflow)
+
+data <- readRDS("data/warehouse/test_lstm.rds")
+
+# Convertir les past_stats en une matrice et diviser en train/test
+X_data <- array(NA, dim = c(nrow(data), 3, length(data$past_stats[[1]])))
+for (i in 1:nrow(data)) {
+  X_data[i,,] <- as.matrix(unlist(data$past_stats[[i]]))
+}
+
+# Extraire les autres variables (age, draft_rank, height) et les répéter 3 fois pour chaque timestep
+X_additional <- aperm(
+  array(
+    rep(array(unlist(data[, c("age", "draft_rank", "height")])), times = 3),
+    dim = c(nrow(data), ncol(data[, c("age", "draft_rank", "height")]), 3)
+  ),
+  perm = c(1, 3, 2)
+)
+
+# Combiner les deux matrices en ajoutant les variables supplémentaires à la dimension des features
+X <- array(c(X_data, X_additional), dim = c(dim(X_data)[1], dim(X_data)[2], dim(X_data)[3] + dim(X_additional)[3]))
+
+# La variable cible : game_score
+y <- as.matrix(data$game_score)
+
+
+# Train model ------------------------------------------------------------
+
+input_layer <- layer_input(shape = c(3, ncol(X)))  # Déclare l'entrée explicitement
+
+output_layer <- input_layer %>%
+  layer_lstm(units = 50, return_sequences = FALSE) %>%
+  layer_dense(units = 1)
+
+model <- keras_model(inputs = input_layer, outputs = output_layer)
+
+# Compilation du modèle
+model %>% compile(
+  loss = 'mean_squared_error',
+  optimizer = 'adam'
+)
+
