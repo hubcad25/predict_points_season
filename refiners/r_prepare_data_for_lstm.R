5cbb47153990205feb2876a91e35e5b19df08eb1
hubcad25 <hubertcadieux@gmail.com>
2024-09-23 14:51:44 -0400

    prep data for lstm

--- refiners/r_prepare_data_for_lstm.R
+++ refiners/r_prepare_data_for_lstm.R
@@ -4,18 +4,23 @@
 library(dplyr)
 
 # Load data --------------------------------------------------------------
-data <- rbind(
-  readRDS("data/marts/data_random_forest_forwards.rds"),
-  readRDS("data/marts/data_random_forest_defensemen.rds")
-)
+data_f <- readRDS("data/marts/data_random_forest_forwards.rds") |> 
+  mutate(
+    ev_icetime = ev_icetime / games_played,
+    pp_icetime = pp_icetime / games_played,
+    game_score = game_score / games_played,
+  )
+
+data_d <- readRDS("data/marts/data_random_forest_defensemen.rds") |> 
+  mutate(
+    ev_icetime = ev_icetime / games_played,
+    pp_icetime = pp_icetime / games_played,
+    game_score = game_score / games_played,
+  )
 
 ### Player infos for fixed variables: age, height, draft_rank, country
 df_player_infos <- readRDS("data/warehouse/player_infos.rds") |> 
-  mutate(
-    forward = ifelse(position %in% c("C", "L", "R"), 1, 0),
-    defenseman = ifelse(position == "D", 1, 0)
-) |> 
-  select(player_id, yob, height, draft_rank, position)
+  select(player_id, yob, height, draft_rank)
 
 # Set variables to include in past stats ----------------------------------------------------------
 variables <- c(
@@ -24,74 +29,55 @@ variables <- c(
   names(ptspredictR::pp_variables)
 )
 
+# Scale variables (and save scaling info) --------------------------------------------------------
+
+scale_f <- scale(data_f[, variables])
+data_f_scaled <- cbind(
+  data_f |> select(-all_of(variables)),
+  as.data.frame(scale_f[, variables])
+)
+scale_info_f <- lapply(variables, function(x) {
+  setNames(c(attr(scale_f, "scaled:center")[x], attr(scale_f, "scaled:scale")[x]), c("mean", "sd"))
+})
+names(scale_info_f) <- variables
+
+saveRDS(scale_info_f, "data/marts/lstm_scale_info_f.rds")
+
+scale_d <- scale(data_d[, variables])
+data_d_scaled <- cbind(
+  data_d |> select(-all_of(variables)),
+  as.data.frame(scale_d[, variables])
+)
+scale_info_d <- lapply(variables, function(x) {
+  setNames(c(attr(scale_d, "scaled:center")[x], attr(scale_d, "scaled:scale")[x]), c("mean", "sd"))
+})
+names(scale_info_d) <- variables
+
+saveRDS(scale_info_d, "data/marts/lstm_scale_info_d.rds")
+
 # Build past stats dataframe ---------------------------------------------
-df_past_stats <- ptspredictR::build_past_stats_df(
-  data = data,
+df_past_stats_f <- ptspredictR::build_past_stats_df(
+  data = data_f_scaled,
   variables = variables,
-  window = 3
+  window = 4
 ) %>%
   # Add player infos
   left_join(., df_player_infos, by = "player_id") |> 
-  filter(season >= 2011) |> 
+  filter(season >= 2012) |> 
   mutate(age = season - yob) |> 
   select(-yob)
 
-# Add targets and save as big json ---------------------------------------
-path <- "data/marts/lstm_data/"
+saveRDS(df_past_stats_f, "data/marts/data_lstm_forwards.rds")
 
-for (i in variables){
-  ## Add target
-  df_target_only <- data |>
-    select(player_id, season, all_of(i))
-  df_with_target <- left_join(
-    df_past_stats,
-    df_target_only,
-    by = c("player_id", "season")
-  )
-  ptspredictR::save_df_past_stats_as_json(
-    df_past_stats = df_with_target,
-    output_file = paste0(path, i, ".rds")
-  )
-}
+df_past_stats_d <- ptspredictR::build_past_stats_df(
+  data = data_d_scaled,
+  variables = variables,
+  window = 4
+) %>%
+  # Add player infos
+  left_join(., df_player_infos, by = "player_id") |> 
+  filter(season >= 2012) |> 
+  mutate(age = season - yob) |> 
+  select(-yob)
 
-saveRDS(df_with_target, file = "data/warehouse/test_lstm.rds")
-
-# Fonction pour structurer les past_stats en vecteurs de 3 éléments dans une liste
-get_past_stats_as_vector <- function(past_stats) {
-  sapply(past_stats[[1]], function(x) as.vector(x), simplify = FALSE)
-}
-
-# Fonction principale pour générer le JSON
-generate_json <- function(df) {
-  # Initialisation d'une liste pour stocker les données finales
-  players_list <- list()
-  
-  # Boucle à travers chaque ligne du dataframe
-  for (i in seq_len(nrow(df))) {
-    player_id <- paste0("player", i)
-    
-    # Extraction des past_stats sous forme de vecteurs de 3 éléments
-    past_stats_vector <- get_past_stats_as_vector(df$past_stats[i])
-    
-    # Création d'un dictionnaire pour le joueur
-    player_data <- list(
-      past_stats = past_stats_vector,
-      target = df$target[i],
-      age = df$age[i],
-      height = df$height[i]
-    )
-    
-    # Ajout des données du joueur à la liste finale
-    players_list[[player_id]] <- player_data
-    message(i)
-  }
-  
-  # Conversion de la liste en JSON
-  json_output <- jsonlite::toJSON(players_list)
-  
-  return(json_output)
-}
-
-json_result <- generate_json(df_with_target)
-
-
+saveRDS(df_past_stats_d, "data/marts/data_lstm_defensemen.rds")
