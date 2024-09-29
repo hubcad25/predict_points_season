6639592bec97b1100e8e2e21bee79b4ff6f88e87
hubcad25 <hubertcadieux@gmail.com>
2024-09-28 20:15:58 -0400

    upgrade e_nhlapi_player_infos to extract all players currently on nhl rosters

--- extractors/e_nhlapi_player_infos.R
+++ extractors/e_nhlapi_player_infos.R
@@ -3,16 +3,26 @@
 # Packages ---------------------------------------------------------------
 library(dplyr)
 
-# Get nhl player_ids to fetch --------------------------------------------
+# Get all nhl player_ids to fetch --------------------------------------------
+### Take all players currently on a NHL roster
+player_ids <- as.character(unlist(sapply(ptspredictR::nhl_api_2024_teams, function(i) {
+  roster <- unname(unlist(ptspredictR::get_team_roster(i)))
+  message(i)
+  return(roster)
+})))
 
-df_skaters <- readRDS("data/warehouse/individual_skaters.rds")
-player_ids <- unique(df_skaters$player_id)
+# Only keep those that are not already in data/lake/nhlapi/player_infos/ ----
+players_already_extracted <- gsub("\\.rds$", "", list.files("data/lake/nhlapi/player_infos"))
+
+## Only extract these players
+players_to_extract <- player_ids[!(player_ids %in% players_already_extracted)]
 
 # Extract json files -----------------------------------------------------
-for (i in seq_along(player_ids)){
-  url <- paste0("https://api-web.nhle.com/v1/player/", player_ids[i], "/landing")
+
+for (i in seq_along(players_to_extract)){
+  url <- paste0("https://api-web.nhle.com/v1/player/", players_to_extract[i], "/landing")
   json_data <- jsonlite::fromJSON(url)
-  saveRDS(json_data, paste0("data/lake/nhlapi/player_infos/", player_ids[i], ".rds"))
+  saveRDS(json_data, paste0("data/lake/nhlapi/player_infos/", players_to_extract[i], ".rds"))
   if (i %% 50 == 0){
     message(i)
   }
